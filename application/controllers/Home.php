9abdf689ad417278eeca64e53ee764a9147278a4
Stephen <ondiekistephen5@gmail.com>
2020-01-21 07:49:13 +0300

    initial commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: https://github.com/davinder17s
+ * Email: davinder17s@gmail.com
+ * Repository: https://github.com/davinder17s/codeigniter-middleware
+ */
+
+class Home extends MY_Controller {
+
+    protected function middleware()
+    {
+        return array('admin_auth');
+    }
+
+    public function index()
+    {
+//        print_r($this->middlewares['admin_auth']->roles);
+		echo $this->middlewares['admin_auth']->name;
+    }
+}
